87d06ac476ad4ccc0edd6c82ae2033adf5646667
vojans1992 <vojans1992@gmail.com>
2023-07-25 16:34:05 +0200

    Initial commit

---
+++ SuperDuperCook/src/main/java/com/tim1/cook/SuperDuperCookApplication.java
@@ -0,0 +1,13 @@
+package com.tim1.cook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDuperCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDuperCookApplication.class, args);
+	}
+
+}
---
+++ SuperDuperCook/src/test/java/com/tim1/cook/SuperDuperCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim1.cook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDuperCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
