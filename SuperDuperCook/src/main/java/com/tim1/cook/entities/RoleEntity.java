8d5d2e10c6b5f90ec9f3f412263776c7135e667e
AleksandraStanisic <125409811+AleksandraStanisic@users.noreply.github.com>
2023-07-25 20:14:35 +0200

    entiteti as

---
+++ SuperDuperCook/src/main/java/com/tim1/cook/entities/RoleEntity.java
@@ -0,0 +1,81 @@
+package com.tim1.cook.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.annotations.ResultCheckStyle;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@Entity
+@Table(name = "role")
+@JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+public class RoleEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	//@SQLDelete(sql = "UPDATE role SET deleted = true WHERE id = ? AND version = ?", check = ResultCheckStyle.COUNT)
+	@Where(clause = "deleted = false")
+	@Column(name = "role_id")
+	private Integer Id;
+	@Column(name = "role_name")
+	private String name;
+	@JsonIgnore
+	@Version
+	private Integer version;
+	
+	
+	@NotFound(action = NotFoundAction.IGNORE)
+	@JsonIgnore
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<UserEntity> users = new ArrayList<>();
+
+
+	public RoleEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	public List<UserEntity> getUsers() {
+		return users;
+	}
+	public void setUsers(List<UserEntity> users) {
+		this.users = users;
+	}		
+}
