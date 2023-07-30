f565a3cecac9553a283556bb237110732ad60514
AleksandraStanisic <125409811+AleksandraStanisic@users.noreply.github.com>
2023-07-30 11:29:28 +0200

    Update AllergenController.java

--- SuperDuperCook/src/main/java/com/tim1/cook/controllers/AllergenController.java
+++ SuperDuperCook/src/main/java/com/tim1/cook/controllers/AllergenController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.tim1.cook.service.AllergenService;
 
 @RestController
 @RequestMapping("/api/v1/allergen")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AllergenController {
 	
 	@Autowired
